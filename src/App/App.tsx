9c68c8217401ab5eb8219875da238e03a628691f
Emanuel-Oliveira <101813364+oZoiko@users.noreply.github.com>
2022-07-02 21:11:37 -0300

    Initial commit

---
+++ src/App/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import {JokeGenerator} from '../components/JokeGenerator/JokeGenerator'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <JokeGenerator/>
+    </>
+    
+  )
+}
+
+export default App
