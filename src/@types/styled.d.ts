dafa47c9aebfc1a3e96e2800877dfd532394253e
Emanuel-Oliveira <101813364+eoliveiradev@users.noreply.github.com>
2022-08-04 19:13:54 -0300

    Initial dist subtree commit

--- src/@types/styled.d.ts
+++
@@ -1,8 +0,0 @@
-import "styled-components"
-import { defaultTheme } from "../themes/defaultTheme"
-
-type ThemeType = typeof defaultTheme;
-
-declare module "styled-components"{
-  export interface DefaultTheme extends ThemeType {}
-}
